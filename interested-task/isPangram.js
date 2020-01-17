a1de5a81d549390a14e4982ca0f070e6464ae59a
Vadim <timchukvadim@gmail.com>
2020-01-17 13:49:45 +0200

    Update isPangram.js

--- interested-task/isPangram.js
+++ interested-task/isPangram.js
@@ -2,17 +2,18 @@ function isPangram(string){
   const aCode = 97;
   const zCode = 122;
   
-  const strArr = string.toLowerCase().split('');
-  const letters = {};
-  
-  strArr.forEach((letter) => {
+  const lettersInStr = string
+  .toLowerCase()
+  .split('')
+  .reduce((result, letter) => {
     const letterCode = letter.charCodeAt(0);
     if (letterCode > aCode - 1 && letterCode < zCode + 1) {
-      letters[letterCode] = letter;
+      result[letterCode] = letter;
     }
-  });
+    return result;
+  }, {});
   
-  return Object.keys(letters).length === 26;
+  return Object.keys(lettersInStr).length === 26;
 }
 
 var string = "The quick brown fox jumps over the lazy dog."
